b1d7ee07c9685baff8e069bf9a80bfa7a5c8e70b
SahakKDev <sahak.keshishyan.dev@gmail.com>
2026-03-25 17:52:18 +0400

    init project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <form>What do you need for your 😍 trip?</form>
+      <main>
+        {/* ITEMS */}
+
+        {/* ACTIONS */}
+      </main>
+
+      <footer>Start adding some items to your packing list 🚀</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className='header'>
+      <span>🌴</span> FAR AWAY <span>💼</span>
+    </header>
+  );
+}
